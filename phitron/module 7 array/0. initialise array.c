b1e9eb85780344a75d648c4c4086c35d122429a7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-11 15:31:27 +0600

    initialisation methods

---
+++ phitron/module 7 array/0. initialise array.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main(){
+    // hard code data
+    int arr[5] = {100, 200, 300, 400, 500};
+    // flexible hard code 
+    int arr2[] = {2, 3, 4, };
+
+    // printf("%d", arr2[4]);// output is 200 -> gets data from another array. stupidity of c 🥴. 
+
+    
+    int arr3[5];
+    // printf("%d", arr3[3]);// returns random data
+
+    int arr4[5] = {1, 2};
+    // printf("%d", arr4[3]);// default value for undefined pos is set to zero
+
+    int arr5[5] = {1};
+    printf("%d", arr5[3]);
+
+    return 0;
+}
