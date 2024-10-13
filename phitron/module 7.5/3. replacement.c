f70617d92a89d44d9281a8f390d3ce9dad110fde
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-13 17:26:44 +0600

    without any array

--- phitron/module 7.5/3. replacement.c
+++ phitron/module 7.5/3. replacement.c
@@ -1,26 +1,24 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/D
+
 # include <stdio.h>
 
 int main(){
-
     int n, t;
     scanf("%d", &n);
     int arr[n];
 
+    // for single line array instant output is buffered
     for (int i=0; i<n; i++){
         scanf("%d", &t);
         if(t<0){
-            arr[i]=2;
+            printf("2 ");
         }
-        else if(t!=0) {
-            arr[i]=1;
+        else if(t!=0){
+            printf("1 ");
         }
         else{
-            arr[i]=0;
+            printf("0 ");
         }
-    }
-
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
     }
 
     return 0;
