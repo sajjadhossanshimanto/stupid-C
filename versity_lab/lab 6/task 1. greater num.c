2965408e27378c79e87d68cf6f6117a0c76dc854
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-21 19:33:00 +0600

    changed according to teachers need

--- versity_lab/lab 6/task 1. greater num.c
+++ versity_lab/lab 6/task 1. greater num.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
-void greater_two(int a, int b, int c, int *greater1, int *greater2){
+void greater_two(int a, int b, int c){
+    printf("2 greater numbrs are ");
     if (a>b && a>c){
-        *greater1 = a;
+        printf("%d ", a);
         if (b>c){
-            *greater2 = b;
+            printf("%d ", b);
         }
         else{
-            *greater2 = c;
+            printf("%d ", c);
         }
     }
     else if (b>a && b>c){
-        *greater1 = b;
+        printf("%d ", b);
         if (a>c){
-            *greater2 = a;
+            printf("%d ", a);
         }
         else{
-            *greater2 = c;
+            printf("%d ", c);
         }
     }
     else{
-        *greater1 = c;
+        printf("%d ", c);
         if (a>b){
-            *greater2 = a;
+            printf("%d ", a);
         }
         else{
-            *greater2 = b;
+            printf("%d ", b);
         }
     }
 }
@@ -36,7 +37,6 @@ int main(){
     printf("Enter 3 numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    int g1, g2;
-    greater_two(a, b, c, &g1, &g2);
-    printf("2 greater numbrs are %d %d", g1, g2);
+    greater_two(a, b, c);
+
 }
