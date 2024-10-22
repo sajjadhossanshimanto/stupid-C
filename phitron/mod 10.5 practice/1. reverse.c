60c0b8c5c41e5c89ca8c41f792ba56f55af1a20b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-22 17:18:11 +0600

    ac.

---
+++ phitron/mod 10.5 practice/1. reverse.c
@@ -0,0 +1,52 @@
+# include <stdio.h>
+
+
+void print_arr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void input_arr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+}
+
+void swap(int arr[], int pos1, int pos2){
+    int t;
+    t = arr[pos1];
+    arr[pos1] = arr[pos2];
+    arr[pos2] = t;
+}
+
+void swipe_to_last(int arr[], int n, int pos){
+    // here n expecte tobe size of array
+    for(int i=pos; i<n-1; i++){
+        swap(arr, i, i+1);
+    }
+}
+
+void reverse(int arr[], int n){
+    // array size is n+1
+    // so last pos is n
+    for(int i=n-1; i>=0; i--){
+        swap(arr, i, n);// to last position
+        swipe_to_last(arr, n+1, i);
+    }
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int arr[n+1];// changes
+    input_arr(arr, n);
+    arr[n]=0;
+
+    reverse(arr, n);
+    print_arr(arr, n);
+
+    return 0;
+}
---
+++ temp.py
@@ -0,0 +1,5 @@
+n = int(input())
+inp = input().split(" ")
+
+
+print(" ".join(inp[::-1]))
