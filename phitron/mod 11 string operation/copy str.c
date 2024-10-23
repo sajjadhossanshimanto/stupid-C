bf1274d14bbe72e481b1e21e98a96c32c0aaa969
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-23 15:51:10 +0600

    strcpy function

--- phitron/mod 11 string operation/copy str.c
+++ phitron/mod 11 string operation/copy str.c
@@ -1,14 +1,18 @@
 # include <stdio.h>
+# include <string.h>
 
 
 int main(){
     char a[10] = "abcd";
-    char copy_a[10];
+    char copy_a[10]="hello";
 
-    for (int i=0; a[i]!='\0'; i++){
-        copy_a[i] = a[i];
-    }
-    // copy_a[i]='\0';//good practise
+    // for (int i=0; a[i]!='\0'; i++){
+    //     copy_a[i] = a[i];
+    // }
+    // // copy_a[i]='\0';//good practise
+
+    //     dst,  source --> ulta
+    strcpy(copy_a, a);
 
     printf("%s", copy_a);
     
