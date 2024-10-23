10cb4520d3004055dc318a6ed256f738c1702ac6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-23 16:09:30 +0600

    concert str

---
+++ phitron/mod 11 string operation/2. concert.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+
+char* concert(char a[], char b[]){
+    int len = strlen(a) + strlen(b)-1;
+    char* new = (char*)malloc(len * sizeof(char));
+    // char new[strlen(a)+strlen(b)-1];
+
+    int i=0;
+    for (; a[i]!='\0'; i++){
+        new[i] = a[i];
+    }
+
+    for (int j=0; b[j]!='\0'; i++, j++){
+        new[i] = b[j];
+    }
+
+    return new;
+}
+
+
+int main(){
+
+    char a[10]="hello";
+    char b[10]=" world";
+
+    printf("%s", concert(a, b));
+    
+    return 0;
+}
