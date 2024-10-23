5821db52047b4cdfb2f8dab05ff6f8a7716705e8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-23 10:27:14 +0600

    riverse one

--- phitron/array.c
+++ phitron/array.c
@@ -30,3 +30,27 @@ void copy_arr(int arr[], int to_arr[], int n){
         to_arr[i] = arr[i];
     }
 }
+
+
+void swap(int arr[], int pos1, int pos2){
+    int t;
+    t = arr[pos1];
+    arr[pos1] = arr[pos2];
+    arr[pos2] = t;
+}
+
+void swipe_to_last(int arr[], int n, int pos){
+    // here n expecte tobe size of array
+    for(int i=pos; i<n-1; i++){
+        swap(arr, i, i+1);
+    }
+}
+
+void reverse_one(int arr[], int n){
+    // array size is n+1
+    // so last pos is n
+    for(int i=n-1; i>=0; i--){
+        swap(arr, i, n);// to last position
+        swipe_to_last(arr, n+1, i);
+    }
+}
