d7abf2f9dd2cf68a4714f32a0d26aea299e340b8
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-29 15:10:14 +0600

    code is ok but wa

--- phitron/mod 14.5 practise/2. count_letters.c
+++ phitron/mod 14.5 practise/2. count_letters.c
@@ -4,7 +4,6 @@
 int main(){
 
     char s[1000001];
-    // fgets(s, 1000001, stdin);
     scanf("%s", &s);
 
     int count[26];
@@ -13,14 +12,14 @@ int main(){
     }
 
     for (int i=0; s[i]!='\0'; i++){
-        // scanf("%c", &c);
-        // if (c=='\0'){
-        //     break;
-        // }
         count[s[i]-'a']+=1;
     }
 
-    printf("%d", count[0]);
+    for (int i=0; i<26; i++){
+        if (count[i]==0) continue;
+
+        printf("%c : %d\n", i+'a', count[i]);
+    }
 
     return 0;
 }
