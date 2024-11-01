7ba3c802ea3487228b6ca73d30c23f6fec53274b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-01 17:08:41 +0600

    ac now issue find by gpt

--- phitron/mod 16 assignment 03/2. count_in_matrix.c
+++ phitron/mod 16 assignment 03/2. count_in_matrix.c
@@ -15,21 +15,18 @@ int main(){
     scanf("%d %d %d", &n, &m, &x);
     int mat[n][m];
 
-    int counter[1001];
-    for (int i=0; i<1001; i++){
-        counter[i] = 0;
-    }
+    int counter[1001]={0};
     for (int i=0; i<n; i++){
         for (int j=0; j<m; j++){
-            scanf("%d", &mat[n][m]);
-            counter[mat[n][m]] +=1;
+            scanf("%d", &mat[i][j]);
+            counter[mat[i][j]] +=1;
         }
     }
 
     int t;
     for (int i=0; i<x; i++){
         scanf("%d", &t);
-        printf("output -> %d\n", counter[t]);
+        printf("%d\n", counter[t]);
     }
     
     return 0;
