3dae5acabebc4a4de431578b57dd20447571fefb
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-06 08:51:22 +0600

    1 - n range func

--- phitron/mod 18 recurion/3. print 1 - N.c
+++ phitron/mod 18 recurion/3. print 1 - N.c
@@ -11,9 +11,19 @@ void func(int n){
     func(n);
 }
 
+void range(int x, int n){
+    if (x>n){
+        return ;
+    }
+
+    printf("%d\n", x);
+    range(++x, n);
+}
+
 int main(){
 
-    func(10);
+    // func(10);
+    range(1, 10);
 
     return 0;
 }
