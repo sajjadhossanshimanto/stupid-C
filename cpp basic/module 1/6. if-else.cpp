41b79b1210e41022b1ebb5d127cfa61d5161c19e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-18 17:49:47 +0600

    if else

---
+++ cpp basic/module 1/6. if-else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// same to same as `c`
+int main(){
+    int x=5;
+
+    if (x%2==1){
+        cout << "odd" << endl;
+    } else{
+        cout << "even" << endl;
+    }
+    
+    return 0;
+}
