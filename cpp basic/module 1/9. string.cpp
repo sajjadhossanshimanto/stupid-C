5b74989a44f565b269db43736d85dbb2e196978a
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-18 18:38:54 +0600

    cin.ignore

--- cpp basic/module 1/9. string.cpp
+++ cpp basic/module 1/9. string.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 
+//fgets aldo works in cpp
 int main(){
+    int x;
+    cin >> x;
+    cin.ignore();// to avoid interaction between cin and getline
+
     char s[100];
     // get input with space
     cin.getline(s, 100);
