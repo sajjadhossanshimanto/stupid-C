316bf972c5b968124e40e84f245ba58f056b1254
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-18 17:15:29 +0600

    variable input

---
+++ cpp basic/module 1/2. input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// using namespace std;
+
+
+int main(){
+    int x;
+    
+    std::cin >> x;
+    std::cout << "the number you entered is: " << x<< std::endl;
+    
+    return 0;
+}
