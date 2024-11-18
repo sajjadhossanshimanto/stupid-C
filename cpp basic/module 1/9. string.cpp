68ff77f39d214ce59fafe983845ac23e60b6ecb6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-18 18:37:01 +0600

    cin.getline

---
+++ cpp basic/module 1/9. string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    char s[100];
+    // get input with space
+    cin.getline(s, 100);
+
+    cout << s << endl;
+    
+    return 0;
+}
