53a97d87ab108e3a1b4457ecb985975e6027ff78
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-21 21:27:33 +0600

    sorting in array

---
+++ cpp basic/mod 3 class/8. sort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_arr(int n, int arr[]) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+    int n = 5;
+    int arr[] = {100, 40, 8, 1, 20};
+    
+    //   start_pointer, end_pointer
+    sort(arr, arr+n);// end point is excludive
+    print_arr(n, arr);
+
+    return 0;
+}
