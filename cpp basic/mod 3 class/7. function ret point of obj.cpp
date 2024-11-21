9f5b12fe6b274170642e34f32c3c167213f1fe08
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-21 20:46:44 +0600

    return pointer of objecct

---
+++ cpp basic/mod 3 class/7. function ret point of obj.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Student{
+    public:
+    int roll;
+    char sec;
+    double gpa;
+
+    Student(int roll, char sec, double gpa){
+        (*this).roll = roll;
+        this->gpa = gpa;
+
+        this->sec = sec;
+    }
+};
+
+Student* func(){
+    Student *karim = new Student(17, 'a', 4.00);
+    return karim;
+}
+
+int main(){
+    
+    Student* s1 = func();
+    cout << (*s1).roll << " " << s1->sec << " " << s1->gpa << endl;
+    // gurbage value printed
+
+    return 0;
+}
