65bcfd1c3548b3fccf9a319ff8635abc15f89bef
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-25 12:19:28 +0600

    modifires part 1

---
+++ cpp basic/mod 5  string class/4. modifire 1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string s = "hello ";
+    cout << s << endl;
+    s.append("world");
+    // s += "world";
+    cout << s << endl;
+
+    // base function for append.
+    s.push_back('1');// pushes only a single char
+    cout << s << endl;
+    s.pop_back();
+    cout << s << endl;
+
+    // strings are mutable-flexible
+    s[0] = 'G';
+    cout << s << endl;
+    
+    return 0;
+}
