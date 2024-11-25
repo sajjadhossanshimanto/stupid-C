10a4d56972fb1d1ae35f4ea3ddf05e8b26d97bac
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-25 12:12:18 +0600

    methods related to indexing

---
+++ cpp basic/mod 5  string class/3. element access.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string s="hello world";
+
+    cout << s << endl;
+    cout << s[4] << endl;
+    cout << s.at(4) << endl;// normally use koeri na
+
+    cout << s.front() << endl;// s[0]
+    cout << s.back() << endl;// actually size -1
+
+    
+    
+    return 0;
+}
