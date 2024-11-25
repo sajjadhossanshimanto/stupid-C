2f96768b3c06c316b0ee2c454c16f56452c376c7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-25 21:31:39 +0600

    input with getline

---
+++ cpp basic/mod 5  string class/7. input str with space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int x;
+    cin >> x;// int 
+    
+    string s;
+    cin.ignore();// to ignore presending enter
+    getline(cin, s);
+
+    cout << s << endl;
+    
+    return 0;
+}
