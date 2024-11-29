5c43ba5ed507cd9041d4ebc8311b3e7974596149
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-29 08:47:06 +0600

    sstring stream reeverse

---
+++ cpp basic/mod 6 string/5. codeforce reverse word.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string s;
+
+    getline(cin, s);
+    stringstream ss(s);
+
+    string word;
+    while (ss>>word){
+        reverse(word.begin(), word.end());
+        cout << word << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
