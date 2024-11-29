18db38eb2ad15ca8fca478dc944e888814730b70
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-29 10:54:49 +0600

    concept

---
+++ cpp basic/mod 2 dynamic memory/6. arr dyn size.cpp
@@ -0,0 +1,8 @@
+// 1. static variable cann't be deleted.
+// delete key only works with dynamic
+
+// process
+// dynamic array manei j existing array size barano habe ta na.
+// 1. notun boro size arr toiry kore 
+// 2. copy all previous data. then extened data
+// jthen we can delete the previous one.
