6b04de9d4aa1a73fa17a2f210d01faf77f053f84
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-11-29 18:41:35 +0600

    lab 1

---
+++ versity_lab/lab 1/2. add num.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(){
+    int integet1;
+    int integet2;
+    int sum;
+
+    integet1 = 55;
+    integet2 = 60;
+
+    sum = integet1+integet2;
+    printf("sum of %d and %d is %d", integet1, integet2, sum);
+    return 0;
+}
