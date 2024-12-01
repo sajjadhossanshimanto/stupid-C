1db405c9c820b3a51763f2971466ac3ab82f7610
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-01 10:49:58 +0600

    questions

--- cpp basic/mod 7.5 practise/1. Even Hate Odd.cpp
+++ cpp basic/mod 7.5 practise/1. Even Hate Odd.cpp
@@ -1,44 +1,44 @@
-    # include <bits/stdc++.h>
-    using namespace std;
+# include <bits/stdc++.h>
+using namespace std;
 
 
-    void input_arr(int n, int arr[]){
-        for(int i=0; i<n; i++){
-            cin >> arr[i];
-        }
+void input_arr(int n, int arr[]){
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
     }
+}
 
-    int main(){
-        int t;
-        cin >> t;
-        for (int i=0; i<t; i++){
-            int n;
-            cin >> n;
-            
-            int arr[n];
-            input_arr(n, arr);
-            if (n&1){
-                cout << -1 << endl;
-                continue;
-            }
-            
-            int odd = 0, even = 0;
-            for (int i: arr){
-                if (i&1){
-                    odd++;
-                }
-                else{
-                    even++;
-                }
-            }
-
-            if (odd==even){
-                cout << 0 << endl;
+int main(){
+    int t;
+    cin >> t;
+    for (int i=0; i<t; i++){
+        int n;
+        cin >> n;
+        
+        int arr[n];
+        input_arr(n, arr);
+        if (n&1){
+            cout << -1 << endl;
+            continue;
+        }
+        
+        int odd = 0, even = 0;
+        for (int i: arr){
+            if (i&1){
+                odd++;
             }
             else{
-                cout << n/2 - min(odd, even) << endl;
+                even++;
             }
-
         }
-        return 0;
+
+        if (odd==even){
+            cout << 0 << endl;
+        }
+        else{
+            cout << n/2 - min(odd, even) << endl;
+        }
+
     }
+    return 0;
+}
