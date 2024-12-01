1b1af43f0dec6c3b3e14254a8fba784bb31ec4c7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-01 10:48:05 +0600

    ac

---
+++ cpp basic/mod 7.5 practise/1. Even Hate Odd.cpp
@@ -0,0 +1,44 @@
+    # include <bits/stdc++.h>
+    using namespace std;
+
+
+    void input_arr(int n, int arr[]){
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+    }
+
+    int main(){
+        int t;
+        cin >> t;
+        for (int i=0; i<t; i++){
+            int n;
+            cin >> n;
+            
+            int arr[n];
+            input_arr(n, arr);
+            if (n&1){
+                cout << -1 << endl;
+                continue;
+            }
+            
+            int odd = 0, even = 0;
+            for (int i: arr){
+                if (i&1){
+                    odd++;
+                }
+                else{
+                    even++;
+                }
+            }
+
+            if (odd==even){
+                cout << 0 << endl;
+            }
+            else{
+                cout << n/2 - min(odd, even) << endl;
+            }
+
+        }
+        return 0;
+    }
