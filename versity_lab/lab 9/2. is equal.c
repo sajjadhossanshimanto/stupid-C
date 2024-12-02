2e74c51258bf80a9ebae027b273876b28dab0073
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-02 06:33:12 +0600

    string compare

---
+++ versity_lab/lab 9/2. is equal.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdio.h>
+
+
+int main(){
+    char s1[100], s2[100];
+
+    printf("enter a string: ");
+    fgets(s1, 100, stdin);
+    
+    printf("enter another string: ");
+    fgets(s2, 100, stdin);
+
+    
+    for (int i=0; s1[i]!='\0' || s2[i]!='\0'; i++){
+        if (s1[i]!=s2[i]){
+            printf("string are not equal.");
+            return 0;
+        }
+    }
+    printf("strings you entered are equal.");
+
+    
+    return 0;
+}
