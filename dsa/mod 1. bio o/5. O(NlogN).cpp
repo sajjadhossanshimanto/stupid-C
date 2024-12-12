693afb012bd199d0cb70af1fc3f83ca7224a0034
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-12 16:10:51 +0600

    O(nlogn)

---
+++ dsa/mod 1. bio o/5. O(NlogN).cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5, m=3;
+
+    for (int i=1; i<=n; i++){// i ++ place e gun ba vag thaakle
+        for (int j=1; j<=m; j*=2){
+            cout << i << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
