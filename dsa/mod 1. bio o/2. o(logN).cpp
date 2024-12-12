f907d07d4a4aa6e2945b4322b1589b1701793225
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-12 15:48:26 +0600

    logn

---
+++ dsa/mod 1. bio o/2. o(logN).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=10;
+
+    for (int i=0; i<n; i*=2){// i ++ place e gun ba vag thaakle
+        cout << i << endl;
+    }
+    return 0;
+}
