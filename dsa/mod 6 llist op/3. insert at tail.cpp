ea7d944971e3d492a9dc202b3d9956095da31ab1
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-16 09:45:13 +0600

    insert node to the back of link list

---
+++ dsa/mod 6 llist op/3. insert at tail.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void print_linklist(Node* head){
+    Node* node = head;// temp variable
+    while (node!=NULL){
+        cout << node->val << " ";
+        node = node->next;
+    }
+}
+
+void insert_back(Node* &head, int val){
+    Node* newnode = new Node(val);
+    
+    Node* tail = head;
+    while (tail->next!=NULL){
+        tail = tail->next;
+    }
+
+    tail->next = newnode;
+}
+
+int main(){
+    Node* head = new Node(10);
+
+    insert_back(head, 20);
+    insert_back(head, 30);
+    insert_back(head, 40);
+    print_linklist(head);
+    
+    return 0;
+}
