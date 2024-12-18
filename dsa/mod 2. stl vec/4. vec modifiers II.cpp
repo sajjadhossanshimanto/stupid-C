ef99a4c45d5655885d526bfdc6d2f2d4334f2510
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-18 21:28:54 +0600

    replace, stupid find

---
+++ dsa/mod 2. stl vec/4. vec modifiers II.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void print_vec(vector<int> v){
+    for (int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    vector<int> v = {1, 2, 3, 4, 2, 3, 2};
+    
+    cout << "original: ";
+    print_vec(v);
+    cout << endl;
+
+    replace(v.begin(), v.end()-1, 2, 200);// end exclusive
+    print_vec(v);
+
+    // stupid find function works with iterable
+    auto it = find(v.begin(), v.end(), 4);
+    if (it==v.end()){
+        cout << "not found";
+    }
+    else{
+        cout << "found";
+    }
+    return 0;
+}
