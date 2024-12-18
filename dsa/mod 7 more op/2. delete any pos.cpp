4f0a616d55996e6dbe1438e6960f1a5647b44d89
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-18 18:36:15 +0600

    delete node with indexing

---
+++ dsa/mod 7 more op/2. delete any pos.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void print_linklist(Node* head){
+    Node* node = head;// temp variable
+    while (node!=NULL){
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void insert_back(Node* &tail, int val){
+    Node* newnode = new Node(val);
+    if (tail==NULL){
+        // passinf head to insert back totally seams ilogical to me
+        tail = newnode;
+        return ;
+    }
+
+    tail->next = newnode;
+    tail = newnode;
+}
+
+
+void delete_head(Node* &head){
+    Node* del_node = head;
+    head = head->next;
+    delete del_node;
+}
+
+void delete_at_pos(Node* &head, int pos){
+    if (pos==0){
+        delete_head(head);
+        return;
+    }
+    Node* parent = head;
+    for(int i=0; i<pos-1; i++){
+        parent = head->next;
+    }
+    
+    Node* del = parent->next ;
+    parent->next = parent->next->next;
+    delete del;
+}
+
+int main(){
+    Node* head = new Node(10);
+    Node* tail = head;
+
+    insert_back(tail, 20);
+    insert_back(tail, 30);
+    insert_back(tail, 40);
+    print_linklist(head);
+
+    delete_at_pos(head, 1);
+    print_linklist(head);
+    
+    return 0;
+}
