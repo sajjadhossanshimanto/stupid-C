525c7a09c908d75affcb83becd84e8f61521ebc7
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-22 18:54:55 +0600

    delete head node

---
+++ dsa/mod 9 duby link list/4. deletion.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head){
+    Node* node = head;
+    while (node!=NULL){
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node* tail){
+    Node* node = tail;
+    while (node!=NULL){
+        cout << node->val << " ";
+        node = node->prev;
+    }
+    cout << endl;
+}
+
+
+void delete_head(Node* &head){
+    Node* temp = head;
+    head->next->prev = NULL;// TODO: cummon mistake
+    head = head->next;
+
+    delete temp;
+}
+
+
+int main(){
+    
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+
+    head->next = a;
+
+    a->next = b;
+    a->prev = head;
+
+    b->next = c;
+    b->prev = a;
+
+    c->prev = b;
+
+    Node* tail = c;
+
+
+    // both prints are necessary to confirm the correct connection both from fond and back
+
+    print_forward(head);
+    print_backward(tail);
+
+    delete_head(head);
+    print_forward(head);
+    print_backward(tail);
+
+    return 0;
+}
