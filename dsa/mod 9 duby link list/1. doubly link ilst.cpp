fc028f99224257e62524ef322c9c8d9531d510dc
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-22 17:46:19 +0600

    doubly link list

---
+++ dsa/mod 9 duby link list/1. doubly link ilst.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+int main(){
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+
+    head->next = a;
+
+    a->next = b;
+    a->prev = head;
+
+    b->next = c;
+    b->prev = a;
+
+    c->prev = b;
+    
+    return 0;
+}
