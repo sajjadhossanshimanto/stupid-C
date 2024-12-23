26d25ffeae0849b00162569c324c1909664da13c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-12-23 12:59:44 +0600

    back, font, index, insert

---
+++ dsa/mod 10 stl list/4. modifier func copy.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void print_list(list<int> l){
+    for (auto i: l){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    list<int> l = {10, 20, 30, 40};
+    print_list(l);
+
+    // back operation
+    l.push_back(500);
+    print_list(l);
+    l.pop_back();
+
+    // font operation
+    l.push_front(0);
+    print_list(l);
+    l.pop_front();
+
+
+    // access element at index
+    print_list(l);
+    cout << *next(l.begin(), 2) << endl;
+    
+    // insert -> single value
+    l.insert(next(l.begin(), 2), -1);
+    print_list(l);
+
+    // inseert series of data
+    vector<int> arr = {-2,-3, -4};
+    l.insert(next(l.begin(), 3), arr.begin(), arr.end());
+    print_list(l);
+
+    return 0;
+}
