9391a45a66d6af3a6fda27afc9b5d4cfced8b7d5
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-05 18:39:50 +0600

    stack use

--- dsa/mod 13 stack implementation/2. stack.cpp
+++ dsa/mod 13 stack implementation/2. stack.cpp
@@ -25,8 +25,27 @@ class MyStack{
 };
 
 int main(){
-    MyStack ms;
+    MyStack st;
     
-    
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    cout << st.top() << endl;
+    st.pop();
+    if (!st.empty()){
+        // to avoid segmentation fault we have to always check before tring to access the top
+        cout << st.top() << endl;
+    }
+    if (!st.empty()){
+        // this check is also must before pop
+        st.pop();
+    }
+
+
     return 0;
 }
