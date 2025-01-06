8ad0f2ab195cb3c56d9d38b9b70f9371cc62754b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-06 16:02:08 +0600

    std queue functions

---
+++ dsa/mod 14 queue implementation/6. std queue.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    queue<int> q;
+    q.back;
+    q.front;
+    
+    q.push;
+    q.pop;
+
+    q.empty;
+    q.size;
+
+    // extra
+    q.swap;
+    q.emplace;
+    
+    return 0;
+}
