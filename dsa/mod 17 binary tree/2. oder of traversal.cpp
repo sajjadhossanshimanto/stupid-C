47741de85f8457a56aa60a9ce19823eccd868cb6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-12 13:04:12 +0600

    notes upon tree traversal oder

---
+++ dsa/mod 17 binary tree/2. oder of traversal.cpp
@@ -0,0 +1,11 @@
+/*
+there are 3 oders:
+ - pre oder
+ - in oder
+ - post oder
+
+these names are based upon the position of root
+- in pre oder the root is placed in the -> root, left, right
+- in oder   -> left root right. root is placed in between
+- post oder -> left right root.
+*/
