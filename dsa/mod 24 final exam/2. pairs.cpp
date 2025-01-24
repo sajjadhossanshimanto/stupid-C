5ac855446c74fe5b8902e9724ec1b3f1bae0e820
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-24 15:53:57 +0600

    array implimentation gets ac

--- dsa/mod 24 final exam/2. pairs.cpp
+++ dsa/mod 24 final exam/2. pairs.cpp
@@ -5,39 +5,31 @@ class Data{
     public:
         string name;
         int serial;
-
-        // Data(string name, int serial){
-        //     this->name = name;
-        //     this->serial = serial;
-        // }
 };
 
-class cmp{
-    public:
-    bool operator()(Data l, Data r){
-        if (l.name==r.name){
-            return l.serial < r.serial;
-        }
-        return l.name > r.name;
+bool cmp(Data l, Data r){
+    if (l.name==r.name){
+        return l.serial > r.serial;
     }
-};
+    return l.name < r.name;
+}
 
 int main(){
     int n;
     cin >> n;
+    Data all_pair[n];
 
-    priority_queue<Data, vector<Data>, cmp> pq;
-    while(n--){
-        Data person;
-        cin >> person.name >> person.serial;
-        pq.push(person);
+    // input
+    for (int i=0; i<n; i++){
+        cin >> all_pair[i].name >> all_pair[i].serial;
     }
     
+    sort(all_pair, all_pair+n, cmp);
 
-    while(!pq.empty()){
-        cout << pq.top().name << " " << pq.top().serial << endl;
-        pq.pop();
+    for (int i=0; i<n; i++){
+        cout << all_pair[i].name << ' ' << all_pair[i].serial << endl;
     }
+
 
     return 0;
 }
