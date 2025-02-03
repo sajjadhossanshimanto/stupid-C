22bbaccd2873855f365e6287b0808338875de4fe
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-03 15:33:05 +0600

    tree vs graph

---
+++ algorithm phitron/mod 1 graph representation/1. tree vs graph.cpp
@@ -0,0 +1,9 @@
+/*
+main and only difference:
+- tree can't cointain any cycle. graph can have cycle
+
+other minor diff:
+- edges in tree is one directional. have a parent to child relation
+- trees edges between a---b means a is connected to b and b is connected to a
+
+*/
