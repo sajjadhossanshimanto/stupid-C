98344736a9e2fb588ab34253dc3ebb818432ff01
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-03 14:29:27 +0600

    comparision

---
+++ algorithm phitron/mod 1 graph representation/7. comparision.cpp
@@ -0,0 +1,17 @@
+/*
+best use cases
+
+# adjacency matrix
+- when we need to know is there anyy edge between a to b
+- existance of an edge in O(1)
+
+
+
+# adjacency list
+- when i need to know all the nodes that are connected to a crtain node
+- 
+
+
+# edge list 
+- when i just need to know all the edge regardless of any certain point
+*/
