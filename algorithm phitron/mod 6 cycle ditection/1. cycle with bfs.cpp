82418e19b57a4b2ec6b7d07d11ef0a19888bf20c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-12 08:56:30 +0600

    1st implimentation

---
+++ algorithm phitron/mod 6 cycle ditection/1. cycle with bfs.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+6 5
+0 1
+1 2
+2 3
+3 0
+4 5
+*/
+const int max_n = 105;
+vector<int> graph[max_n];
+bool vis[max_n];
+
+bool bfs_cycle(int node){
+    queue<int> q;
+    int parent[max_n];// i may have use a queue of pair <node:int, parent:int>
+    memset(parent, -1, sizeof(parent));
+
+    q.push(node);
+    vis[node] = true;
+
+    while (!q.empty()){
+        node = q.front();
+        q.pop();
+
+        for (int i: graph[node]){
+            if (vis[i] && i!=parent[i]){
+                return true;
+            }
+            if (vis[i]) continue;
+            vis[i] = true;
+
+            parent[i] = node;
+            q.push(i);
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+
+    while (q--){
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    // default values
+    memset(vis, false, sizeof(vis));
+
+    // for different connected component
+    for (int i=0; i<n; i++){
+        if (!vis[i]){
+            cout << bfs_cycle(i);
+        }
+    }
+    
+    return 0;
+}
