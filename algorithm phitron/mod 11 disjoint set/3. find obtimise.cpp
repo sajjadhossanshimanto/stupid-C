19a12318d146f10e2bdd0d979d405f7bb0cf5d6c
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-04 14:03:35 +0600

    path compressation

---
+++ algorithm phitron/mod 11 disjoint set/3. find obtimise.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int parent[1005];
+
+
+int find(int node){
+    if (parent[node]==-1){
+        return node;
+    }
+    
+    int par = find(parent[node]);
+    parent[node] = par;
+    
+    return par;
+}
+
+int main(){
+    memset(parent, -1, sizeof(parent));
+
+    // input -> by hand
+    parent[0] = 1;
+    parent[2] = 1;
+    parent[3] = 1;
+    parent[4] = 5;
+    parent[5] = 3;
+
+    cout << find(4) << endl;// parent is 1
+    
+    // verity
+    for (int i=0; i<5; i++){
+        cout << "parent of " << i << "is " << parent[i] << endl;
+    }
+
+    return 0;
+}
