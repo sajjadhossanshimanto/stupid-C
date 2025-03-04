b1d85a4e270849b67d9858d846bda9461552204d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-04 15:08:50 +0600

    ac

--- algorithm phitron/mod 11.5 practise/2. number of boundry.py
+++ algorithm phitron/mod 11.5 practise/2. number of boundry.py
@@ -5,7 +5,7 @@ from typing import List
 
 
 move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
+inf = float("inf")
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         gx, gy = len(grid), len(grid[0])
@@ -14,26 +14,29 @@ class Solution:
             grid[x][y] = 0
 
             cell = 1
-            edge_touched = True
             for cx, cy in move:
                 cx, cy = x+cx, y+cy
                 if cx<0 or cy<0 or cx>=gx or cy>=gy: 
-                    edge_touched = True
+                    cell = inf
                     continue
                 if grid[cx][cy]==0: continue
 
-                count, edge_touched = dfs(cx, cy)
-                cell += count
+                cell += dfs(cx, cy)
 
-            return cell, edge_touched 
+            return cell
 
 
         cell_count = 0
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if grid[x][y]==1:
-                    count, edge = dfs(x, y)
-                    if not edge:
+                    count = dfs(x, y)
+                    if count!=inf:
                         cell_count+=count
         
         return cell_count
+
+s = Solution()
+# %%
+s.numEnclaves([[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]])
+# %%
