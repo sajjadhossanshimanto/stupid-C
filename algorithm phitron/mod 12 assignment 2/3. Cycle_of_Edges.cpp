a5b64a412b8901d8eac48bb3584453f0cb5c5360
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-05 17:14:24 +0600

    back edge countere with dj set

---
+++ algorithm phitron/mod 12 assignment 2/3. Cycle_of_Edges.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int parent[100005];
+int find(int a){
+    if (parent[a]==-1){
+        return a;
+    }
+
+    int par = find(parent[a]);
+    parent[a] = par;// path compression
+    return par;
+}
+
+bool ds_union(int a, int b){
+    int par_a = find(a);
+    int par_b = find(b);
+
+    if (par_a==par_b) return true;
+    parent[a] = b;// unconditional
+    return false;
+}
+
+int main(){
+    memset(parent, -1, sizeof(parent));
+
+    int n, e;
+    cin >> n >> e;
+    
+    int cycle = 0;
+    while (e--){
+        int a, b;
+        cin >> a >> b;
+        if (ds_union(a, b)){
+            cycle += 1;
+        }
+    }
+
+    cout << cycle;
+    
+    return 0;
+}
