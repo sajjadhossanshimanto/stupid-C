32b483a466985e96d861c21ef9d8f5485909f31b
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-07 17:12:05 +0600

    ac with memoization

--- algorithm phitron/mod 16 final exam/3. Make_It.cpp
+++ algorithm phitron/mod 16 final exam/3. Make_It.cpp
@@ -4,16 +4,23 @@ using namespace std;
 int cache[100005];
 
 bool solve(int n){
-    if (n==1) return true;
+    if (cache[n]!=-1) return cache[n];
     if (n<=0) return false;
 
-    if (solve(n-3)) return true;
-    if ((n&1)==0 && solve(n/2)) return true;
+    bool ans = false;
+    if (solve(n-3)) ans = true;
+    if ((n&1)==0 && solve(n/2)) ans = true;
 
-    return false;
+    cache[n] = ans;
+    return ans;
 }
 
 int main(){
+    // set default values
+    memset(cache, -1, sizeof(cache));
+    cache[0] = 0;
+    cache[1] = true;
+
     int t, n;
     cin >> t;
     while(t--){
